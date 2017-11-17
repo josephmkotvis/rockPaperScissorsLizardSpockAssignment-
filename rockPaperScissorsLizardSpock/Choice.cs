dda7089a3ea0253f903a7701401019ba24a6034e
josephmkotvis <josephmkotvis@gmail.com>
2017-11-17 10:28:40 -0600

    made check choice methods, add player or comp score to Choice class

--- rockPaperScissorsLizardSpock/Choice.cs
+++ rockPaperScissorsLizardSpock/Choice.cs
@@ -8,5 +8,31 @@ namespace rockPaperScissorsLizardSpock
 {
     class Choice
     {
+        string "q" = string "Rock";
+        string "w" = string "Paper";
+        string "e" = string "Scissors";
+        string "r" = string "Lizard";
+        string "t" = string "Spock";
+
+        public void CheckChoicePVE()
+        {
+
+        }
+        public void CheckChoicePVP()
+        {
+
+        }
+        public void AddPlayer1Score()
+        {
+
+        }
+        public void AddPLayer2Score()
+        {
+
+        }
+        public void AddComputerScore()
+        {
+
+        }
     }
 }
